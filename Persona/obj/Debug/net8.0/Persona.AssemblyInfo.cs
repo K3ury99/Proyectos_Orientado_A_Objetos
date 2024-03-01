fbb1bd3f49d535692a01d9d15a7ae1fc60251ffc
K3ury <keury2004@hotmail.com>
2024-03-01 12:16:34 -0400

    Probando pull request 3

--- Persona/obj/Debug/net8.0/Persona.AssemblyInfo.cs
+++ Persona/obj/Debug/net8.0/Persona.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persona")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae4efbbfa37a9d3b3042e5e4937a29e1fc7d3a4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd7a76c1f12bbf179256a062a943ceb1b6d5ebe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persona")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persona")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
