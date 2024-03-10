4e8bba02433450b503fcea3f36fe18d848355135
K3ury <keury2004@hotmail.com>
2024-03-10 17:21:49 -0400

    Proyecto: Contador, Agregado y finalizado

---
+++ Contador/Contador.cs
@@ -0,0 +1,59 @@
+namespace Contador.Contador
+{
+    public class Contador
+    {
+        // atributos de la clase contador
+        public double Valor { get; set; }
+
+        // Constructor por defecto
+        public Contador(){
+            this.Valor = 0;
+        }
+
+        // Constructor con atributos
+        public Contador(double Valor){
+            this.Valor = Valor;
+        }
+
+        public void Consultar_Valor()
+        {
+            Console.WriteLine($"Valor actual: {Valor}");
+        }
+
+        public void Incrementar(double cantidad)
+        {
+            if (cantidad < Valor)
+            {
+                Console.WriteLine($"Error: No se puede ingresar un valor menor a {Valor}");
+            }
+            else
+            {
+                Valor += cantidad;
+                Console.WriteLine($"Valor incrementado con exito. Nuevo Valor: {Valor}");
+            }
+        }
+
+        public void Decrementar(double cantidad)
+        {
+            if (cantidad <= 0)
+            {
+                Console.WriteLine("Error: La cantidad debe ser mayor que cero.");
+            }
+            else if (cantidad > Valor)
+            {
+                Console.WriteLine($"Error: No se puede ingresar un valor mayor a {Valor}");
+            }
+            else
+            {
+                Valor -= cantidad;
+                Console.WriteLine($"Valor Decrementado con éxito. Nuevo Valor: {Valor}");
+            }
+        }
+
+        public void Reiniciar()
+        {
+            Valor = 0;
+            Console.WriteLine($"Contador reiniciado. Nuevo Valor: {Valor}");
+        }
+    }
+}
---
+++ Contador/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Contador.Contador
+{
+    class Program
+    {
+        public static Contador contador1 = new Contador();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("""
+                Bienvenido al contador
+                
+                ¿Que desea hacer?
+                
+                1) Consultar el valor.
+                2) Incrementar el valor.
+                3) Decrementar el valor.
+                4) Reiniciar el contador.
+                0) Salir del contador.    
+                """);
+
+                Console.Write("---> ");
+                int opcion = Convert.ToInt32(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 0:
+                        return;
+                    case 1: //Consulta de Valor
+                        contador1.Consultar_Valor();
+                        break;
+                    case 2: //Incremento
+                        Console.Write("\nCuanto quiere incrementar?: ");
+                        int incrementado = Convert.ToInt32(Console.ReadLine());
+                        contador1.Incrementar(incrementado);
+                        break;
+                    case 3: //Decremento
+                        Console.Write("\nCuanto quiere decrementar?: ");
+                        int decrementado = Convert.ToInt32(Console.ReadLine());
+                        contador1.Decrementar(decrementado);
+                        break;
+                    case 4: // Reiniciar
+                        contador1.Reiniciar();
+                        break;
+                    default:
+                        Console.WriteLine("Error, Introduzca un numero dentro del rango.");
+                        break;
+                }
+            //Para volver a empezar
+            Console.WriteLine("\n---¿Desea realizar otra operacion? (s/n): ");
+            string ContinueOption = Console.ReadLine().ToLower();
+            
+            if(ContinueOption != "s")
+            {
+                Console.WriteLine("---Saliendo del Programa");
+                break;
+            }
+            }
+        }
+    }
+    
+}
